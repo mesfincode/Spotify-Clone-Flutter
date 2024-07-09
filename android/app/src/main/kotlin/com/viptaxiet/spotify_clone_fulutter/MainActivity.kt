9c192e5e3e683ebbade6b67ef36516ffe4f23687
Mesfin Girma <mesfingirmatwo@gmail.com>
2024-07-09 09:26:38 +0300

    implemented the folder structure and color theme

---
+++ android/app/src/main/kotlin/com/viptaxiet/spotify_clone_fulutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.viptaxiet.spotify_clone_fulutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const primary = Color(0xff42C83C);
+  static const lightBackground = Color(0xffF2F2f2);
+  static const darkBackground = Color(0xff0d0c0c);
+  static const grey = Color(0xffBEBEBe);
+  static const darkGrey = Color(0xff343434);
+}
